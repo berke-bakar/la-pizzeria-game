f095babe02a56200628e78fc66d25bbb374a9a1d
Berke Bakar <88384613+berke-bakar@users.noreply.github.com>
2024-09-07 22:31:19 +0300

    -> Setup physics materials contact properties

--- context/PhysicsProvider.tsx
+++ context/PhysicsProvider.tsx
@@ -8,7 +8,16 @@ export const PhysicsProvider = ({ children }: React.PropsWithChildren) => {
   const [world] = useState(() => new CANNON.World());
 
   useEffect(() => {
-    world.broadphase = new CANNON.NaiveBroadphase();
+    world.broadphase = new CANNON.SAPBroadphase(world);
+    world.defaultMaterial = new CANNON.Material({
+      friction: 10,
+      restitution: 0,
+    });
+    world.defaultContactMaterial = new CANNON.ContactMaterial(
+      world.defaultMaterial,
+      world.defaultMaterial,
+      { restitution: 0, friction: 10 }
+    );
     world.gravity.set(0, -10, 0);
   }, [world]);
 
