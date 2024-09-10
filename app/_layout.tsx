1c439f2acfdaa4db2a600397a3e4067859df3bb2
Berke Bakar <88384613+berke-bakar@users.noreply.github.com>
2024-09-10 15:56:36 +0300

    -> Forced mobile to use landscape mode

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,10 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="game" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false, orientation: "landscape" }}
+      />
     </Stack>
   );
 }
