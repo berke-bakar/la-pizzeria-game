a890b8145e5f7b1e3314c23e19bfd464f397d2ed
Berke Bakar <88384613+berke-bakar@users.noreply.github.com>
2024-09-10 16:10:12 +0300

    -> Improved suspense progress tracker

--- components/SuspenseProgress.tsx
+++ components/SuspenseProgress.tsx
@@ -1,16 +1,28 @@
 import { useEffect } from "react";
 import { useProgress } from "@react-three/drei";
-import { useAtom } from "jotai";
-import { progressAtom } from "@/constants/constants";
+import { useAtom, useSetAtom } from "jotai";
+import { currentSceneAtom, progressAtom } from "@/constants/constants";
 
 type Props = {};
 
 const SuspenseProgress = (props: Props) => {
   const { progress } = useProgress();
-  const [_, setProgress] = useAtom(progressAtom);
+  const setProgress = useSetAtom(progressAtom);
+  const setCurrentScene = useSetAtom(currentSceneAtom);
   useEffect(() => {
     if (progress) {
-      setProgress(progress);
+      if (
+        100 - Number.EPSILON <= progress &&
+        progress >= 100 + Number.EPSILON
+      ) {
+        setCurrentScene((prev) => ({
+          currentScene: prev.currentScene,
+          transitionNeeded: false,
+        }));
+        setProgress(0);
+      } else {
+        setProgress(progress);
+      }
     }
   }, [progress]);
 
