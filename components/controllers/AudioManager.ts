c8524d4f3941cc4c7c42d7830610a652ecb25cd5
Berke Bakar <88384613+berke-bakar@users.noreply.github.com>
2024-09-20 10:29:19 +0300

    -> Updated AudioManager to handle multiple sounds

--- components/controllers/AudioManager.ts
+++ components/controllers/AudioManager.ts
@@ -2,8 +2,8 @@ import { Audio } from "expo-av";
 
 class AudioManager {
   private static instance: AudioManager;
-  private sound: Audio.Sound | null = null;
-  private isLoaded = false;
+  private sounds: Record<string, Audio.Sound> = {};
+  private isLoaded: Record<string, boolean> = {};
 
   private constructor() {}
 
@@ -14,29 +14,29 @@ class AudioManager {
     return AudioManager.instance;
   }
 
-  async loadAudio(fileUri: string) {
-    if (!this.isLoaded) {
+  async loadAudio(fileUri: string, trackName: string) {
+    if (!this.isLoaded[trackName]) {
       try {
         const { sound } = await Audio.Sound.createAsync(
           { uri: fileUri },
           { shouldPlay: false }
         );
-        this.sound = sound;
-        this.isLoaded = true;
+        this.sounds[trackName] = sound;
+        this.isLoaded[trackName] = true;
       } catch (error) {
         console.error("Error loading audio:", error);
       }
     }
   }
 
-  async playSection(start: number, duration: number) {
-    if (this.sound) {
+  async playSection(trackName: string, start: number, duration: number) {
+    if (this.sounds[trackName]) {
       try {
-        await this.sound.setPositionAsync(start * 1000); // Start in ms
-        await this.sound.playAsync();
+        await this.sounds[trackName].setPositionAsync(start * 1000); // Start in ms
+        await this.sounds[trackName].playAsync();
 
         setTimeout(async () => {
-          await this.sound?.stopAsync();
+          await this.sounds[trackName]?.stopAsync();
         }, duration * 1000); // Duration in ms
       } catch (error) {
         console.error("Error playing section:", error);
@@ -44,10 +44,10 @@ class AudioManager {
     }
   }
 
-  async unloadAudio() {
-    if (this.sound && this.isLoaded) {
-      await this.sound.unloadAsync();
-      this.isLoaded = false;
+  async unloadAudio(trackName: string) {
+    if (this.sounds[trackName] && this.isLoaded[trackName]) {
+      await this.sounds[trackName].unloadAsync();
+      this.isLoaded[trackName] = false;
     }
   }
 }
